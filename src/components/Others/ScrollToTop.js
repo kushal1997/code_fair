56fd00035e0b4368adfe312322efb459e0194417
kushal1997 <kushalrao103@gmail.com>
2023-04-10 16:28:13 +0530

    Home Page is completed

---
+++ src/components/Others/ScrollToTop.js
@@ -0,0 +1,6 @@
+
+export const ScrollToTop = () => {
+  return (
+    <div>ScrollToTop</div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export {HomePage} from "./Home/HomePage";
+export {ProductsList} from "./Products/ProductsList";
+
+
+
