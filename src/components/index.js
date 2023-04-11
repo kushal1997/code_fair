e70dbd9afb121c246d087d4ea863c67114e995a0
kushal1997 <kushalrao103@gmail.com>
2023-04-12 00:52:34 +0530

    added scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export {Footer} from "../components/Layouts/Footer"
 
 export {ProductCard} from "./Elements/ProductCard"
 export {Rating} from "./Elements/Rating"
+
+export {ScrollToTop} from "./Others/ScrollToTop"
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+
+import { ScrollToTop } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <ScrollToTop />
+      <App />
     </Router>
   </React.StrictMode>
 );
